03dad6a47ac22b501e6a60e6529b6f0ba750a0cf
SammySlimJ <samanthasalimu@gmail.com>
2024-12-05 22:34:01 +0200

    Implement Task 0 again

--- binary_trees.h
+++ binary_trees.h
@@ -17,10 +17,10 @@
  */
 struct binary_tree_s
 {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
+ int n;
+ struct binary_tree_s *parent;
+ struct binary_tree_s *left;
+ struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
@@ -36,9 +36,9 @@ typedef struct binary_tree_s heap_t;
  */
 typedef struct link_s
 {
-  size_t n:
-  struct binary_tree_s const *node;
-  struct link_s *next;
+ size_t n:
+ struct binary_tree_s const *node;
+ struct link_s *next;
 }link_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
